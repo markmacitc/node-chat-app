e264e45e4b1f419de7ffba1df33d44a629072255
Mark McDonald <longryder@gmail.com>
2018-03-14 19:44:09 +0000

    Greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -2,17 +2,12 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createEmail', {
-    to: 'joebloggs@example.com',
-    text: 'Hey. This is Andrew.'
-  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-socket.on('newEmail', function (email) {
-  console.log('New email', email);
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
 });
